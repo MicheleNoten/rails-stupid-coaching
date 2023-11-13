3f0f9e84ef5ebff396ddef562311a732acae3472
Michele Noten <notenm@gmail.com>
2023-11-13 12:30:28 +0800

    updated questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,14 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @response = params[:question]
-    case @response
-    when "I am going to work"
-      @answer = "Great!"
-    when @response.include?('?')
-      @answer = "Silly question, get dressed and go to work"
+    if @response == 'I am going to work'
+      @answer = 'Great!'
+    elsif @response.include?('?')
+      @answer = 'Silly question, get dressed and go to work'
     else
-     @answer = "I don't care, get dressed and go to work"
+      @answer = "I don't care, get dressed and go to work"
     end
   end
-
 end
